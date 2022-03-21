e7b7485fc24077dc1e71e6b9babfed039199ace1
Steven Kristjan Rekand <s.rekand@khk.ee>
2022-03-21 12:12:59 +0200

    post request use

--- index.js
+++ index.js
@@ -2,21 +2,33 @@ const express = require('express')
 const path = require("path");
 const app = express();
 
+const bodyParser = require('body-parser')
+const {use} = require("express/lib/router");
+app.use(bodyParser.urlencoded({extended:true}));
+
 // adds views directory path
 app.set('views', path.join(__dirname, 'views'));
 // adds views template engine
 app.set('view engine', 'ejs');
 
 
-app.get('/questions', (req, res) => {
-    let questions = [
-        {title : "What is Node.js?", user : "Kadi", votes : "10"},
-        {title : "What is Express.js?", user : "Mike", votes : "8"}
-    ]
+app.get('/login', (req, res) => {
+    res.render('login')
+})
 
-    res.render('index', {questions : questions})
+app.post('/login', (req, res) => {
+    let username = req.body.username;
+    let password = req.body.password;
+
+    if (username === 'user' && password === 'qwerty'){
+        res.redirect('/dashboard');
+    }
+});
+
+app.get('/dashboard', (req, res) => {
+    res.render('dashboard');
 })
 
 app.listen(3000, () => {
-    console.log('Server started on http://localhost:3000');
+    console.log('Server started on http://localhost:3000/login');
 });
