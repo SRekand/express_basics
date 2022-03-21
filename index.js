26afe18044aed005f765bdc8c1f040e1be0a8feb
Steven Kristjan Rekand <s.rekand@khk.ee>
2022-03-21 11:46:13 +0200

    template loop use

--- index.js
+++ index.js
@@ -2,18 +2,20 @@ const express = require('express')
 const path = require("path");
 const app = express();
 
-// adds public directory
-app.use(express.static('public'));
 // adds views directory path
 app.set('views', path.join(__dirname, 'views'));
 // adds views template engine
 app.set('view engine', 'ejs');
 
 
-app.get('/user/:username', (req, res) => {
-    let user = req.params.username;
-    res.render('index.ejs', {username : user});
-});
+app.get('/questions', (req, res) => {
+    let questions = [
+        {title : "What is Node.js?", user : "Kadi", votes : "10"},
+        {title : "What is Express.js?", user : "Mike", votes : "8"}
+    ]
+
+    res.render('index', {questions : questions})
+})
 
 app.listen(3000, () => {
     console.log('Server started on http://localhost:3000');
