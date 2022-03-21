1ef80ce05b49822a8b7c585e437edf025d4db8d8
Steven Kristjan Rekand <s.rekand@khk.ee>
2022-03-21 10:52:07 +0200

    Basic express application

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 const express = require('express')
 const app = express();
 
-// set up brouther address row router
+// set up routher address row router
 app.get('/', (req, res)=> {
     // create html responce
-    res.send('<a href="/contact">Contact us</a> <br> <a href="/about">About us</a>');
+    res.send('<a href="/contact">Contact us</a> <br> <a href="/about">About us</a> <br> <a href="/homepage">Home Page</a>');
 });
-
+// Contacts
 app.get('/contact', (req, res)=> {
     res.send('<h1>Contact us page</h1>');
 })
-
+// Homepage
+app.get('/homepage', (req, res)=> {
+    res.send('<h1>This is a HomePage</h1>')
+})
+// About page
 app.get('/about', (req, res)=> {
     res.send('<h1>About us page</h1>');
 })
-
+// Error page kui ei leia ülesse aadressi
 app.get('*', (req, res)=> {
     res.send('404. This page does not exist. <a href="/">Go to Homepage</a>')
 })
