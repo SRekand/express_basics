6ff2d4b5a33c06d10bf98907d3972ad1d03b41c8
Steven Kristjan Rekand <s.rekand@khk.ee>
2022-03-21 11:19:57 +0200

    Basic use of EJS

--- index.js
+++ index.js
@@ -1,29 +1,11 @@
 const express = require('express')
 const app = express();
 
-// set up routher address row router
-app.get('/', (req, res)=> {
-    // create html responce
-    res.send('<a href="/contact">Contact us</a> <br> <a href="/about">About us</a> <br> <a href="/homepage">Home Page</a>');
+app.get('/user/:username', (req, res) => {
+    let user = req.params.username;
+    res.render('index.ejs', {username : user});
 });
-// Contacts
-app.get('/contact', (req, res)=> {
-    res.send('<h1>Contact us page</h1>');
-})
-// Homepage
-app.get('/homepage', (req, res)=> {
-    res.send('<h1>This is a HomePage</h1>')
-})
-// About page
-app.get('/about', (req, res)=> {
-    res.send('<h1>About us page</h1>');
-})
-// Error page kui ei leia ülesse aadressi
-app.get('*', (req, res)=> {
-    res.send('404. This page does not exist. <a href="/">Go to Homepage</a>')
-})
 
-// listen app via part
-app.listen(3000, ()=> {
+app.listen(3000, () => {
     console.log('Server started on http://localhost:3000');
 });
